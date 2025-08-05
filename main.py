6b322e550363706c83a051550f110fe1dea936db
Akshansh <akshanshz@gmail.com>
2025-08-05 19:07:46 +0200

    adding url func

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -5,6 +6,39 @@ load_dotenv()  # loads .env and sets api key
 client = OpenAI()  # reads api key
 
 
+def get_website_html(url: str) -> str:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # raise error for bad responses
+        return response.text
+    except requests.RequestException as e:
+        print(f"Error fetching the url {url}: {e}")
+        return ""
+
+
+def extract_core_website_content(html: str) -> str:
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=f"""
+            You are an expert web content extractor. Your task is to extract the core content from a given HTML page.
+            The core content should be the main text, excluding navigation, footers, and other non-essential elements like scripts etc.
+            
+            Here is the HTML content:
+            <html>
+            {html}
+            </html>
+            
+            Please extract the core content and return it as plain text.
+        """
+    )
+    return response.output_text
+
+
+def summarize_content(content: str) -> str:
+    response = client.responses.create(
+        model="gpt-4o-mini",
+
+    )
 def generate_x_post(usr_input: str) -> str:
     prompt = f"""
         You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).

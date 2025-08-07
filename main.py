99eae9549cf31a5332a19ecb3e8e6f593dad9ace
Akshansh <akshanshz@gmail.com>
2025-08-07 22:38:13 +0200

    call_function_start

--- main.py
+++ main.py
@@ -14,13 +14,38 @@ def get_temperature(city: str) -> float:
     return 20.0
 
 
+available_functions = {
+    "get_temperature": get_temperature
+}
+
+tools = [
+    {
+        "type": "function",
+        "name": "get_temperature",
+        "description": "Get current temperature for a given location.",
+        "parameters": {
+                "type": "object",
+                "properties": {
+                    "city": {
+                        "type": "string",
+                        "description": "City for which to get the temperature."
+                    }
+                },
+            "additionalProperties": False,
+            "required": ["city"],
+        }
+
+    }
+]
+
+
 def main():
     print("Hello from agent")
     user_input = input("Your question: ")
     prompt = f"""
-    you are a helpful assistant. Answer the user;s question in a friendlu way.
+    you are a helpful assistant. Answer the user;s question in a friendly way.
     you can also use tools if oyu feel like they help you provide a better answer:
-        - get_temperature(city: str) -> float: Get the current temperture for a given city.
+        - get_temperature(city: str) -> float: Get the current temperature for a given city.
         
     If you want to use one of these tools, you should output the tool name and its arguments in the following format:
         tool_name: arg1, arg2, ....
@@ -50,7 +75,7 @@ def main():
         {user_input}
         </user-question>
         
-        You requested to use the get_temperature tool for the city "{arg}".
+        
         Here's the result of using that tool:
         The current temperature in {arg} is {temperature}°C.
         """

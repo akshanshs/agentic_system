49b1b14f936514ea0d0d39a878b5c4d1966ac333
Akshansh <akshanshz@gmail.com>
2025-08-07 17:16:51 +0200

    tempreture_demo_tool

--- main.py
+++ main.py
@@ -5,35 +5,65 @@ load_dotenv()  # loads .env and sets api key
 client = OpenAI()  # reads api key
 
 
-def generate_x_post(usr_input: str) -> str:
-    prompt = f"""
-        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
-
-        Your task is to generate a post that is concise, impactful, and tailored to the topic provided by the user.
-        Avoid using hashtags and lots of emojis (a few emojis are okay, but not too many).
-
-        Keep the post short and focused, structure it in a clean, readable way, using line breaks and empty lines to enhance readability.
-
-        Here's the topic provided by the user for which you need to generate a post:
-        <usr_input>
-        {usr_input}
-        </usr_input>
+def get_temperature(city: str) -> float:
     """
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": usr_input}
-        ],
-        max_tokens=300
-    )
+    Get current temperature for a given city.
+    :param city:
+    :return:
+    """
+    return 20.0
 
-    return response.choices[0].message.content.strip()
 
 def main():
-    usr_input = input("What should the post be about? ")
-    x_post = generate_x_post(usr_input)
-    print("The generated post is")
-    print(x_post)
+    print("Hello from agent")
+    user_input = input("Your question: ")
+    prompt = f"""
+    you are a helpful assistant. Answer the user;s question in a friendlu way.
+    you can also use tools if oyu feel like they help you provide a better answer:
+        - get_temperature(city: str) -> float: Get the current temperture for a given city.
+        
+    If you want to use one of these tools, you should output the tool name and its arguments in the following format:
+        tool_name: arg1, arg2, ....
+    for example:
+        get_temperature: Berlin
+    with that in mind, answer the user's question:
+    <user-question>
+    {user_input}
+    </user-question>
+    
+    If you request a tool, please output ONLY the tool call (as described above) and nothing else.
+    """
+
+    response = client.responses.create(
+        model='gpt-4o-mini',
+        input=prompt
+    )
+    reply = response.output_text
+
+    if reply.startswith("get_temperature"):
+        arg = reply.split(":", 1)[1].strip()
+        temperature = get_temperature(arg)
+        prompt = f"""
+        You are a helpful assistant. Answer the user's question in a friendly way.
+        Here's the user's question:
+        <user-question>
+        {user_input}
+        </user-question>
+        
+        You requested to use the get_temperature tool for the city "{arg}".
+        Here's the result of using that tool:
+        The current temperature in {arg} is {temperature}°C.
+        """
+
+        response = client.responses.create(
+           model="gpt-4o-mini",
+           input=prompt
+        )
+        reply = response.output_text
+        print(reply)
+    else:
+        print(reply)
+
 
 if __name__ == '__main__':
     main()
